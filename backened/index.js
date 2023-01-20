8432ed62862d6c1905f9f81b7c915d664f30edc8
Rajiv Ravindra pathak <70104100+rajivravindrapathak@users.noreply.github.com>
2023-01-20 02:34:50 -0800

    Add files via upload

--- backened/index.js
+++ backened/index.js
@@ -1,1 +1,19 @@
+console.log("a")
 
+const express = require("express")
+
+const app = express()
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.send("home page")
+})
+
+app.post("/signup", (req, res) => {
+    const { email, password} = req.body
+})
+
+app.listen(8004, ()=> {
+    console.log("listining on port 8004")
+})
